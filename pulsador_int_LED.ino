32cbe18106d04a211dcb64a06fc9c4ee23834609
Leonardo-Garcia-Guillen <76006460+Leonardo-Garcia-Guillen@users.noreply.github.com>
2020-12-22 18:56:09 +0100

    Add files via upload

--- pulsador_int_LED.ino
+++ pulsador_int_LED.ino
@@ -35,26 +35,41 @@ ICACHE_RAM_ATTR void RTI() {
   ultima_int = ahora;
 }
 
-int salida = 2; //GPIO 2
-int estado_led=0;
-bool cambia=false;
+int salida = 16; //GPIO 16 - LED de ABAJO
+//int BUILTIN_LED = 0 // GPIO 0 - LED de ARRIBA
+int estado_led0=1;
+int estado_led16=1;
+
+
 /*------------------ SETUP --------------------*/
 void setup() {
-  pinMode(boton_flash, INPUT_PULLUP);
+  pinMode(boton_flash, INPUT_PULLUP); 
+
   // descomentar para activar interrupción
   attachInterrupt(digitalPinToInterrupt(boton_flash), RTI, CHANGE);
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println();
   Serial.println("Interrupción preparada...");
-  pinMode(salida, OUTPUT); 
+  pinMode(salida, OUTPUT); // GPIO 16
+  pinMode(BUILTIN_LED, OUTPUT); // GPIO 0
 
 }
-
+int cont=0;
+bool cuenta_pulsos=true;
+bool actualiza=false;
+unsigned long inicio = 0; 
+bool pulsaciones=false;
 /*------------------- LOOP --------------------*/
 void loop() {
   
-  if (interrupcion==true)
-  {      
+  // INTERRUPCIÓN
+    if (interrupcion==true)
+  {
+    if (cuenta_pulsos=true)
+    {
+      inicio=ahora;
+      cuenta_pulsos=false;
+    }
     if (estado_int==LOW)
     {
       Serial.print("Se detectó una pulsación en millis = ");
@@ -66,27 +81,68 @@ void loop() {
       Serial.print("Terminó la pulsación, duración = ");
       Serial.print(ahora-penultima_int);
       Serial.println(" ms");
-      cambia=true;
-      if (estado_led==1)
-        estado_led=0;
-      else
-        estado_led=1;
-    } 
-    interrupcion=false; // DESABILITAMOS LA INTERRUPCIÓN HASTA QUE VUELVA A HABER UNA LLAMADA A LA MISMA
-    }
-    if (cambia==true)
-    {
-      if (estado_led==1)
+      cont++;
+      if (ahora-penultima_int < 3000) // Mientras NO haya actualización
       {
-        digitalWrite(salida, HIGH);
+        pulsaciones=true;
       }
       else
-      { 
-        digitalWrite(salida, LOW); 
-      }
-        cambia=false;
+        actualiza=true;
     }
-    delay(10);
+    interrupcion=false; // DESABILITAMOS LA INTERRUPCIÓN HASTA QUE VUELVA A HABER UNA LLAMADA A LA MISMA
     
-  }
+    }
+    if (actualiza==true) // mantenemos pulsado: ACTUALIZAMOS! 
+    {
+        Serial.println("ACTUALIZACIÓN");
+        actualiza=false;
+    }
+    
+    unsigned long now=millis();
+   
+      if (now-inicio>650)
+      {
+        if (cont==2) // 2 pulsos - encendemos arriba
+        {
+        Serial.println("DOBLE PULSACION");
+        if (estado_led16==1)
+          {
+            digitalWrite(BUILTIN_LED, HIGH);
+            estado_led16=0;
+          }
+          else
+          { 
+            digitalWrite(BUILTIN_LED, LOW);
+            estado_led16=1;
+          }
+          cont=0;
+          cuenta_pulsos=true;
+        }
+        if (cont==1)
+        {
+        Serial.println("UNA PULSACION");
+        if (estado_led0==1)
+          {
+            digitalWrite(salida, HIGH);
+            estado_led0=0;
+          }
+          else
+          { 
+            digitalWrite(salida, LOW);
+            estado_led0=1;
+          }
+          cont=0;
+          cuenta_pulsos=true;
+        }
+        else if (cont>2)
+        {
+          Serial.println("MAS DE 2 PULSACIONES, CÓDIGO NO VÁLIDO");
+
+          cont=0;
+          cuenta_pulsos=true;
+        }
+    delay(10);
+    }
+}
+  
   
